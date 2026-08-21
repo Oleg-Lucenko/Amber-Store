2ccb12591fd69351e500e9dd7cf34e3f32261403
Oleg-Lucenko <lucenko5577@gmail.com>
2026-08-21 19:00:17 +0300

    feat(Footer) Improve layout Add responsive layout Add a better short description text

--- src/widgets/layouts/footer/Footer.tsx
+++ src/widgets/layouts/footer/Footer.tsx
@@ -1,66 +1,73 @@
-import styles from './footer.module.scss';
-import Link from 'next/link';
+import type { Category } from "@entities/category/types";
 import Image from "next/image";
-import type { Category } from '@entities/category/types';
+import Link from "next/link";
+import styles from "./footer.module.scss";
 
+export default async function Footer({
+  categories,
+}: {
+  categories: Category[];
+}) {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.amberAbout}>
+        <Link href="/" aria-label="Amber home">
+          <Image
+            src="/img/logo2.png"
+            height={120}
+            width={140}
+            className={styles.logo2}
+            alt="Amber"
+          />
+        </Link>
+        <p>
+          Amber is an independent electronics store focused on reliable
+          everyday tech, fair prices, and straightforward service.
+        </p>
+      </div>
 
+      <nav aria-label="Product categories" className={styles.categories}>
+        <h3>Products</h3>
+        <ul>
+          <li>
+            <Link href="/all-products">All products</Link>
+          </li>
+          {categories.map((category) => (
+            <li key={category.id}>
+              <Link href={`/${category.slug}`}>{category.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
 
+      <nav aria-label="Store pages" className={styles.pages}>
+        <h3>Store pages</h3>
+        <ul>
+          <li>
+            <Link href="#">About us</Link>
+          </li>
+          <li>
+            <Link href="#">FAQs</Link>
+          </li>
+          <li>
+            <Link href="#">Delivery information</Link>
+          </li>
+        </ul>
+      </nav>
 
-
-export default async function Footer({categories}: {categories: Category[]}) {
-
-    return(
-        <footer className={styles.footer}>
-        
-            <div className={styles.amberAbout}>
-                <Link href="/"><Image src="/img/logo2.png" height={120} width={140} className={styles.logo2} alt='logo'/></Link>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex sapiente, nam numquam suscipit laudantium, quae quos vero possimus natus excepturi itaque, sit non ad eveniet aperiam reprehenderit doloribus quasi.</p>
-            </div>
-            
-            <nav aria-label='Footer navigation' className={styles.navigation}>           
-                <section className={styles.categories}>
-                    <h3>Products</h3>
-                            <ul>
-                                <li key={1}>
-                                        <Link href={'/all-products'} className={styles.navLink}>
-                                            All products
-                                        </Link>
-                                </li>
-
-                                {categories.map(category => (
-                                            <li key={category.id}>
-                                                <Link href={`/${category.name}`} className={styles.navLink}>
-                                                    {category.name}
-                                                </Link>
-                                            </li>
-                                        ))  
-                                }
-
-                            </ul>
-                </section>
-        
-                <section className={styles.pages}>
-                    <h3>Store pages</h3>
-                    <ul>
-                        <li><Link href='#'>About us</Link></li>
-                        <li><Link href='#'>FAQs</Link></li>
-                        <li><Link href='#'>Delivery information</Link></li>
-                    </ul>
-                </section>
-            </nav> 
-    
-    
-            <section className={styles.contacts}>
-                <h3>Get in touch</h3>
-
-                <ul>
-                    <li><Link href="tel:0333-700-54-16">0333 700 54 16</Link></li>
-                    <li><Link href="mailto:amberstore@gmail.com">amberstore@gmail.com</Link></li>
-                </ul>
-                
-            </section>
-        
-        </footer>
-
-    );
-};
+      <section className={styles.contacts}>
+        <h3>Get in touch</h3>
+        <ul>
+          <li>
+            <Link href="tel:0333-700-54-16">0333 700 54 16</Link>
+          </li>
+          <li>
+            <Link href="mailto:amberstore@gmail.com">
+              amberstore@gmail.com
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </footer>
+  );
+}
