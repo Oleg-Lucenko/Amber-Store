b4e4de6265ae83a90eb2e3f56ae9ff82516a84c5
Oleg-Lucenko <lucenko5577@gmail.com>
2026-08-21 18:01:45 +0300

    feat(product-list-item) Add format price

--- src/entities/product/ui/productListItem/ProductListItem.tsx
+++ src/entities/product/ui/productListItem/ProductListItem.tsx
@@ -1,7 +1,7 @@
 import type { ProductListItemProps } from "../../types";
+import { formatDollars } from "@shared/lib/money";
 import Image from "next/image";
 import Link from "next/link";
-import type { ProductListItemProps } from "../../types";
 import styles from "./product-list-item.module.scss";
 
 
@@ -15,15 +15,14 @@ export function ProductListItem({
   actions,
 }: ProductListItemProps) {
     return (
-
         <li className={styles.productItem}>
             <Link href={`/${categorySlug}/${slug}`} className={styles.productLink}>
                 <Image src={imgPath} width={120} height={120} alt={name} />
                 <span className={styles.productName}>{name}</span>
-                <span className={styles.productPrice}>$ {priceDollars}</span>            
+                <span className={styles.productPrice}>{formatDollars(priceDollars)}</span>            
             </Link>
             {actions}
         </li>
-
     )
 };
+
