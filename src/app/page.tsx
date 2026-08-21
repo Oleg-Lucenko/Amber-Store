86cb18adc0c8f6297635d23f2548290e40aab5a0
Oleg-Lucenko <lucenko5577@gmail.com>
2026-08-21 17:22:03 +0300

    Add product catalog to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-'use client';
-
 import DiscountsSlider from "@widgets/discount-items/DiscountsSlider";
+import { ProductsList } from "@widgets/products-list/ProductsList";
+import { getAllProducts } from "@entities/product/actions";
+import { NoProducts } from "@entities/product";
 
- 
+
+export default async function HomePage() {
+  const products = await getAllProducts();
 
 
-
-export default function HomePage() {
-
+  if (products.length === 0) {
+    return <NoProducts />;
+  }
 
   return (
-    <DiscountsSlider/>
+    <>
+      <DiscountsSlider />
+      <ProductsList listName={'All products'} products={products} />
+    </>
   );
-};
-
+}
